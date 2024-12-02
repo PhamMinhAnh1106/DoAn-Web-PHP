38f5ccbc2e501b7e80a0a4676a8dd1512cf1f383
Pham Minh Anh <135544447+PhamMinhAnh1106@users.noreply.github.com>
2024-12-02 14:44:44 +0700

    Delete bookstore directory

--- bookstore/admin/js/myfuncs.js
+++
@@ -1,8 +0,0 @@
-function xoa(ml)
-{
-	con=confirm("Ban co chac muon xoa khong?");
-	if(con)
-	{
-		location.href="index.php?mo=loai&ac=xoa&ml="+ml;
-	}
-}
--- bookstore/xllogout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['cart']=array();
-unset($_SESSION['user']);
-echo  1;
